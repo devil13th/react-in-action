0c115c0536e4a2250373c241a30dce8bb46a5378
devil13th <181907667@qq.com>
2018-06-15 17:22:07 +0800

    ..

--- webpack-demos/demo07/webpack.config.js
+++ webpack-demos/demo07/webpack.config.js
@@ -1,22 +1,25 @@
-
-var htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  //mode:生产模式production  开发模式development
-  mode:"development", 
-  entry: './main.js',
+  entry: 'index.js',
   output: {
-    filename: './bundle.js'
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
   },
-  plugins:[
-    new htmlWebpackPlugin({
-        filename:'./dist/index.html', //通过模板生成的文件名
-        template:'index.html',//模板路径
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
+
+
+
+    /*new HtmlWebpackPlugin({
+        filename:'./index.html', //通过模板生成的文件名
+        template:'./index.html',//模板路径
         inject:true, //是否自动在模板文件添加 自动生成的js文件链接
         title:'这个是WebPack Demo',
         minify:{
             removeComments:true //是否压缩时 去除注释
         }
-    })
-  ]
-};
+    })*/
+   
