e034e7994a3c14bc7e982caddfd4d3a507bb00f8
devil13th <181907667@qq.com>
2018-07-13 08:56:48 +0800

    ...

--- webpack-demos/demo02/readme.md
+++ webpack-demos/demo02/readme.md
@@ -16,3 +16,26 @@ module.exports = {
 ```
 
 output.filename中的[name]为entry对象的属性名称(bundle1,bundle2)
+
+
+# 打包前先清空文件夹插件 clean-webpack-plugin
+```
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+  //mode:生产模式production  开发模式development
+  mode:"development", 
+  entry: {
+    bundle1: './main1.js',
+    bundle2: './main2.js'
+  },
+  output: {
+    filename: './[name]-[hash].bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist') 
+  ]
+};
+```
+
+
+# devtool的使用
--- webpack-demos/demo07/webpack.config.js
+++ webpack-demos/demo07/webpack.config.js
@@ -1,5 +1,7 @@
+//拷贝首页，并添加js引用 、压缩、替换文本等功能
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+//该插件功能是打包前先清空目标文件夹
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: './main.js',
   output: {
@@ -11,13 +13,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename:'./index.html', //通过模板生成的文件名
         template:'./index.html',//模板路径
-        inject:true, //是否自动在模板文件添加 自动生成的js文件链接的位置
+        inject:true, //是否自动在模板文件添加 自动生成的js文件链接的位置 允许插件修改哪些内容，true/'head'/'body'/false,
         title:'这个是WebPack Demo',
         sss:"xxx",
         minify:{
           removeComments:true //是否压缩时 去除注释
         }
-    })
+    }),
+    new CleanWebpackPlugin('dist') 
   ]
 }
 
