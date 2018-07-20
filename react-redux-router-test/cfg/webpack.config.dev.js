36628811501c8e2260d14a131cdb207601808610
devil13th <181907667@qq.com>
2018-07-20 23:34:08 +0800

    ..

--- react-redux-router-test/cfg/webpack.config.dev.js
+++ react-redux-router-test/cfg/webpack.config.dev.js
@@ -1,9 +1,12 @@
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
+
 // __dirname是node.js的全局变量，它指向当前执行脚本所在的目录。
 module.exports ={
   //devtool: 'cheap-module-source-map',
+  //devtool:false,
   devtool:'eval-source-map',
   entry:[__dirname + '/../src/index.js'],
+
   output:{
     path:__dirname+'/../public',
     filename:'bundle.js'
@@ -39,18 +42,10 @@ module.exports ={
     contentBase: __dirname+'/../public',//本地服务器所加载的页面所在的目录
     historyApiFallback: true,//不跳转
     inline: true//实时刷新
-  }
-  ,
+  },
+
+  
   module:{
-
-
-
-    
-    
-    
-
-
-
 
     rules:[
      
@@ -104,10 +99,8 @@ module.exports ={
         ]
       }
       
-
-   
-
     ]
-  }
+  },
+  
 
 }
