1bb3accba8286d953b94a822b1e6778a43f4d631
devil13th <181907667@qq.com>
2018-08-24 17:27:00 +0800

    创建外部引入react的实际开发例子

---
+++ webpack-demos/reactUMD/readme.md
@@ -0,0 +1,6 @@
+# 外部引用react.js
+html中使用script标签引用react等第三方JS后 将业务代码单独打包后引入html
+
+# 如何兼容IE
+1. 加入polyfill.js
+2. webpack中使用babel
---
+++ webpack-demos/reactUMD/src/main.js
@@ -0,0 +1,17 @@
+
+class MyApp extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <h1>   
+                Split Code
+            </h1>
+        )
+    }
+}
+
+
+ReactDOM.render(<MyApp/>,document.getElementById("app"));
