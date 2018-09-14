40072ebc3f3c3b33790e3b9a1cc7893c3ff2c745
devil13th <181907667@qq.com>
2018-09-14 18:23:21 +0800

    ..

--- devil13th/src/routes/appRouter.js
+++ devil13th/src/routes/appRouter.js
@@ -21,63 +21,65 @@ class AppRouter extends React.Component {
     
       render() {
         return (
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sider
-              collapsible
-              collapsed={this.state.collapsed}
-              onCollapse={this.onCollapse}
-            >
+
+          <Layout>
+            <Header className="header">
               <div className="logo" />
-              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1">
-                  <Icon type="pie-chart" />
-                  <span>Option 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="desktop" />
-                  <span>Option 2</span>
-                </Menu.Item>
-                <SubMenu
-                  key="sub1"
-                  title={<span><Icon type="user" /><span>User</span></span>}
-                >
-                  <Menu.Item key="3">Tom</Menu.Item>
-                  <Menu.Item key="4">Bill</Menu.Item>
-                  <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={<span><Icon type="team" /><span>Team</span></span>}
-                >
-                  <Menu.Item key="6">Team 1</Menu.Item>
-                  <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9">
-                  <Icon type="file" />
-                  <span>File</span>
-                </Menu.Item>
+              
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                style={{ lineHeight: '64px' }}
+              >
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
               </Menu>
-            </Sider>
+            </Header>
             <Layout>
-              <Header style={{ background: '#fff', padding: 0 }} />
-              <Content style={{ margin: '0 16px' }}>
+              <Sider width={200} style={{ background: '#fff' }}>
+                <Menu
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  style={{ height: '100%', borderRight: 0 }}
+                >
+                  <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                    <Menu.Item key="1">option1</Menu.Item>
+                    <Menu.Item key="2">option2</Menu.Item>
+                    <Menu.Item key="3">option3</Menu.Item>
+                    <Menu.Item key="4">option4</Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                    <Menu.Item key="5">option5</Menu.Item>
+                    <Menu.Item key="6">option6</Menu.Item>
+                    <Menu.Item key="7">option7</Menu.Item>
+                    <Menu.Item key="8">option8</Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                    <Menu.Item key="9">option9</Menu.Item>
+                    <Menu.Item key="10">option10</Menu.Item>
+                    <Menu.Item key="11">option11</Menu.Item>
+                    <Menu.Item key="12">option12</Menu.Item>
+                  </SubMenu>
+                </Menu>
+              </Sider>
+              <Layout style={{ padding: '0 24px 24px' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
-                  <Breadcrumb.Item>User</Breadcrumb.Item>
-                  <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                  <Breadcrumb.Item>Home</Breadcrumb.Item>
+                  <Breadcrumb.Item>List</Breadcrumb.Item>
+                  <Breadcrumb.Item>App</Breadcrumb.Item>
                 </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                  <div style={{height:100,border:"1px solid red",overflow:"auto"}}>
-                  1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>
-                  </div>
-                  <Icon type="user" /> User
-                  <img src={img_yay_src}/>
-                </div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>
-                Ant Design ©2018 Created by Ant UED
-              </Footer>
+                <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                <img src={img_yay_src}/>
+                </Content>
+              </Layout>
             </Layout>
           </Layout>
+
+
+
         );
       }
 }
