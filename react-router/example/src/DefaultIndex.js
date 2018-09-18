5e14e1e5feee14d80872ef5900a1b1891e7382ed
十三妖 <181907667@qq.com>
2018-09-19 00:07:23 +0800

    添加知识点位置表格

--- react-router/example/src/DefaultIndex.js
+++ react-router/example/src/DefaultIndex.js
@@ -7,7 +7,76 @@ class DefaultIndex extends Component {
       return (
         <div>
            <h1>default page </h1>   
-           
+           <table  >
+							<tbody>
+								<tr>
+									<th  >
+										<span>知识点</span>
+									</th>
+									<th  >
+										<span>文件位置</span>
+									</th>
+								</tr>
+							</tbody>
+							<tbody >
+								<tr >
+									<td >
+										嵌套路由
+									</td>
+									<td >
+										index.js | Home.js | About.js
+									</td>
+								</tr>
+								<tr >
+									<td >
+									exact属性
+									</td>
+									<td >
+										Home.js
+									</td>
+								</tr>
+								<tr >
+									<td >
+									Switch组件
+									</td>
+									<td >
+										Home菜单(Home.js).js
+									</td>
+								</tr>
+								<tr>
+									<td >
+								Redirect属性
+									</td>
+									<td >
+										Home菜单(Home.js)
+									</td>
+								</tr>
+								<tr>
+									<td >
+									props.match属性
+									</td>
+									<td >
+										param菜单 (Param.js)
+									</td>
+								</tr>
+								<tr>
+									<td >
+									Link NavLink组件
+									</td>
+									<td >
+										home菜单(Home.js)
+									</td>
+								</tr>
+								<tr >
+									<td >
+									Link NavLink组件
+									</td>
+									<td >
+										NotFount菜单
+									</td>
+								</tr>
+							</tbody>
+						</table>
         </div>
       ); 
     }
