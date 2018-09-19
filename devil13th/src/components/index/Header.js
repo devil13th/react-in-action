66aa6b160107e6e5441410db595a528e74f0b8c6
十三妖 <181907667@qq.com>
2018-09-20 00:20:57 +0800

    添加路由菜单

--- devil13th/src/components/index/Header.js
+++ devil13th/src/components/index/Header.js
@@ -37,7 +37,6 @@ class Header extends React.Component{
                         mode="horizontal"
                     >
                         <Menu.Item key="Index" title="首页">
-                            
                             <Link to="/"><Icon type="home" /> Index</Link>
                         </Menu.Item>
                         <Menu.Item key="Antd" title="Antd">
@@ -47,18 +46,29 @@ class Header extends React.Component{
                         
                         
                         <SubMenu title={<span className="submenu-title-wrapper"><Icon type="setting" />React Example</span>}>
-                            
-                                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                <SubMenu title={<span className="submenu-title-wrapper"><Icon type="tool" />Antd</span>}>
-                                    <MenuItemGroup title="Item 1">
-                                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-                                    </MenuItemGroup>
-                                    <MenuItemGroup title="Item 2">
-                                        <Menu.Item key="setting:3">Option 3</Menu.Item>
-                                        <Menu.Item key="setting:4">Option 4</Menu.Item>
-                                    </MenuItemGroup>
-                                </SubMenu>
+                            <Menu.Item key="setting:a">Option</Menu.Item>
+                            <SubMenu title={<span className="submenu-title-wrapper"><Icon type="tool" />Basic</span>}>
+                                <Menu.Item key="setting:0">Option 0</Menu.Item>
+                                <MenuItemGroup title="Item 1">
+                                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                                </MenuItemGroup>
+                                <MenuItemGroup title="Item 2">
+                                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                                </MenuItemGroup>
+                            </SubMenu>
+
+                            <SubMenu title={<span className="submenu-title-wrapper"><Icon type="tool" />Antd</span>}>
+                                <MenuItemGroup title="Item 1">
+                                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                                </MenuItemGroup>
+                                <MenuItemGroup title="Item 2">
+                                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                                </MenuItemGroup>
+                            </SubMenu>
                             
                             <MenuItemGroup title="Item 2">
                                 <Menu.Item key="setting:3">Option 3</Menu.Item>
--- devil13th/src/routes/Index.js
+++ devil13th/src/routes/Index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Layout} from 'antd';
-import { routerRedux, Route,Switch } from 'dva/router';
+import { Layout,Breadcrumb } from 'antd';
+import { routerRedux, Route, Switch, HashRouter } from 'dva/router';
 import Header from '../components/index/Header';
 import Content from '../components/index/Content';
+
+import ContentIndex from '../components/index/ContentIndex';
 import Footer from '../components/index/Footer';
 
 import AntdRouter from '../routes/AntdRouter'
@@ -17,6 +19,8 @@ import img_yay_src from '../imgs/yay.jpg';
 import AntdIndex2 from '../components/antd/AntdIndex2';
 import AntdIndex3 from '../components/antd/AntdIndex3';
 
+const { Content: AntdContent } = Layout;
+
 class Index extends React.Component {
     state = {
         collapsed: false
@@ -27,39 +31,47 @@ class Index extends React.Component {
         this.setState({ collapsed });
     }
 
-   
-      
+
+
 
     render() {
         return (
-            <Layout>
-                <Header 
-                    top={0}
-                    current="Index"
-                >
-                </Header>
-                {/*
-                <Content
-                    path={this.props.Index.path}
-                >  
-                </Content>
-                */}
+            <HashRouter>
+                <Layout>
+                    <Header
+                        top={0}
+                        current="Index"
+                    >
+                    </Header>
+                    {/*
+                    <Content
+                        path={this.props.Index.path}
+                    >  
+                    </Content>
+                    */}
+                    <AntdContent style={{ padding: "0 0px" }}>
+                {/*<Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>{this.props.path}</Breadcrumb.Item>
+                        </Breadcrumb>*/}
+                        <div>
+                            <Switch>
+                                <Route exact path="/" component={ContentIndex} />
+                                <Route exact path="/antd" component={AntdIndex} />
+                                <Route path="/antd/x2" component={AntdIndex2} />
+                                <Route path="/antd/x3" component={AntdIndex3} />
+                            </Switch>
+                        </div>
+                    </AntdContent>
 
-                <div>
-                    <Switch>
-                    <Route exact path="/" component={Content} />
-                    <Route exact path="/antd" component={AntdIndex} />
-                    <Route  path="/antd/x2" component={AntdIndex2} />
-                    <Route  path="/antd/x3" component={AntdIndex3}/>
-                    </Switch>
-                </div>
 
 
-                   
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©2018 Created by Ant UED
-                </Footer>
-            </Layout>
+{/*
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2018 Created by Ant UED
+                    </Footer>*/}
+                </Layout>
+            </HashRouter>
         );
     }
 }
@@ -68,7 +80,7 @@ class Index extends React.Component {
 Index.propTypes = {
 };
 
-export default connect(({Index,loading }) => ({
+export default connect(({ Index, loading }) => ({
     Index,
-    loading 
-  }))(Index);
+    loading
+}))(Index);
